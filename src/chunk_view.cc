aa67a4f5ff9b13b6aa555cf3a41a2cb725cb848b
James Harrison <00jamesh@gmail.com>
2021-05-25 16:25:11 +0100

    Support reading samples from chunk files

---
+++ src/chunk_view.cc
@@ -0,0 +1,123 @@
+#include "chunk_view.h"
+
+#include "index.h"
+
+SampleIterator::SampleIterator(Decoder& dec, size_t sampleCount)
+    : sampleCount(sampleCount), dec(dec), bits(dec) {
+    advance();
+}
+
+bool SampleIterator::next(Sample& s) {
+    if (currentIndex == sampleCount) {
+        return false;
+    }
+    if (currentIndex == 0) {
+        prev.ts = s.timestamp = dec.read_varint();
+        auto val = dec.read_int<uint64_t>();
+        prev.value = s.value = reinterpret_cast<double&>(val);
+
+    } else if (currentIndex == 1) {
+        prev.tsDelta = dec.read_varuint();
+        prev.ts = s.timestamp = prev.ts + prev.tsDelta;
+
+        s.value = readValue();
+    } else {
+        s.timestamp = readTS();
+        s.value = readValue();
+    }
+
+    ++currentIndex;
+    return true;
+}
+
+int64_t SampleIterator::readTS() {
+    auto dod = readTSDod();
+    prev.tsDelta += dod;
+    prev.ts += prev.tsDelta;
+    return prev.ts;
+}
+
+int64_t SampleIterator::readTSDod() {
+    uint8_t tsPrefix;
+    for (int i = 0; i < 4; ++i) {
+        tsPrefix <<= 1;
+        if (!bits.readBit()) {
+            break;
+        }
+        tsPrefix |= 1;
+    }
+
+    // determine how many bits the ts is encoded in from the
+    // prefix. These value ranges match those set in Prometheus' xor.go
+    uint8_t tsBitCount = 0;
+    switch (tsPrefix) {
+    case 0x00: // 0
+        // dod is zero
+        return 0;
+    case 0x02: // 10
+        tsBitCount = 14;
+        break;
+    case 0x06: // 110
+        tsBitCount = 17;
+        break;
+    case 0x0e: // 1110
+        tsBitCount = 20;
+        break;
+    case 0x0f: // 1111
+        tsBitCount = 64;
+        break;
+    }
+
+    size_t tsBits = bits.readBits(tsBitCount);
+
+    // handle negative valuer
+    if (tsBits > (1 << (tsBitCount - 1))) {
+        return tsBits - (1 << tsBitCount);
+    }
+    return tsBits;
+}
+
+double SampleIterator::readValue() {
+    if (!bits.readBit()) {
+        // delta is zero
+        return prev.value;
+    }
+
+    if (!bits.readBit()) {
+        // reuse previous leading/trailing, saves 10 bits
+    } else {
+        // read leading/trailing
+        prev.leading = bits.readBits(5);
+        uint8_t sigBits = bits.readBits(6);
+        if (sigBits == 0) {
+            // 64 would overflow, and 0 is otherwise unused, so 0 is used
+            // instead to encode 64.
+            sigBits = 64;
+        }
+        prev.trailing = 64 - prev.leading - sigBits;
+    }
+
+    uint8_t sigBits = 64 - prev.leading - prev.trailing;
+    size_t newValueBits = bits.readBits(sigBits);
+    newValueBits <<= prev.trailing;
+    newValueBits ^= reinterpret_cast<uint64_t&>(prev.value);
+    double newValue = reinterpret_cast<double&>(newValueBits);
+
+    prev.value = newValue;
+
+    return newValue;
+}
+
+ChunkView::ChunkView(ChunkFileCache& cfc, const ChunkReference& chunkRef)
+    : dec(cfc.get(chunkRef.getSegmentFileId())) {
+    dec.seekg(chunkRef.getOffset());
+    dataLen = dec.read_varuint();
+
+    auto encoding = dec.read_int<uint8_t>();
+    if (encoding != 1) {
+        throw std::runtime_error("Chunk file has unknown encoding");
+    }
+
+    sampleCount = dec.read_int<uint16_t>();
+    dataOffset = dec.tellg();
+}
---
+++ src/chunk_view.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "bit_decoder.h"
+#include "chunk_file_cache.h"
+#include "generator_iterator.h"
+
+class ChunkReference;
+
+struct Sample {
+    int64_t timestamp;
+    double value;
+};
+
+struct SampleIterator : public generator_iterator<SampleIterator, Sample> {
+    SampleIterator(Decoder& dec, size_t sampleCount);
+
+    bool next(Sample& s);
+
+private:
+    double readValue();
+
+    int64_t readTS();
+    int64_t readTSDod();
+
+    struct {
+        int64_t ts;
+        int64_t tsDelta;
+        double value;
+        uint8_t leading;
+        uint8_t trailing;
+    } prev;
+    size_t currentIndex = 0;
+    size_t sampleCount;
+    Decoder& dec;
+    BitDecoder bits;
+};
+
+// non-copying type, caller must ensure the cached chunk file handle
+// outlives a ChunkView instance
+class ChunkView {
+public:
+    ChunkView(ChunkFileCache& cfc, const ChunkReference& chunkRef);
+
+    SampleIterator samples() {
+        return {dec, sampleCount};
+    }
+
+    size_t dataLen;
+    size_t dataOffset;
+
+    size_t sampleCount;
+
+    Decoder dec;
+};
