40a38a4fccef1f1273e3a2b1f0499c2a21ce247b
James Harrison <00jamesh@gmail.com>
2021-05-25 16:21:45 +0100

    Add BitDecoder, a bitwise decoder wrapper
    Will be used when decoding sample data from within chunks. Samples may
    be encoded with variable bit width fields which in some cases are most
    conveniently read bit by bit.

---
+++ src/bit_decoder.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "decoder.h"
+
+class BitDecoder {
+public:
+    BitDecoder(Decoder& dec) : dec(dec){};
+
+    uint64_t readBits(size_t count) {
+        if (count > 64) {
+            throw std::logic_error("Only support reading 64 bits at a time");
+        }
+
+        size_t result = 0;
+        while (count > 0) {
+            if (remainingBits == 0) {
+                dec.read_int_to(buffer);
+                remainingBits = 8;
+            }
+            auto bitsToRead = std::min(count, size_t(remainingBits));
+
+            result <<= bitsToRead;
+            result |= getBitsFromBuffer(bitsToRead);
+            count -= bitsToRead;
+            remainingBits -= bitsToRead;
+        };
+
+        return result;
+    }
+
+    bool readBit() {
+        return bool(readBits(1) & 1);
+    }
+
+protected:
+    static uint8_t getMask(size_t bitCount) {
+        return (uint8_t(1) << bitCount) - 1;
+    }
+
+    uint8_t getBitsFromBuffer(size_t bitCount) {
+        auto mask = getMask(bitCount);
+        mask <<= (remainingBits - bitCount);
+        auto value = buffer & mask;
+        value >>= (remainingBits - bitCount);
+        return value;
+    }
+    Decoder& dec;
+    uint8_t buffer;
+    uint8_t remainingBits = 0;
+};
