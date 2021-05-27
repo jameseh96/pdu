4aaba4926ab87c7cca4df8c635ebf7603704bd2d
James Harrison <00jamesh@gmail.com>
2021-05-27 13:27:02 +0100

    Uninline BitDecoder methods

--- src/bit_decoder.cc
+++ src/bit_decoder.cc
@@ -1,1 +1,42 @@
 #include "bit_decoder.h"
+
+BitDecoder::BitDecoder(Decoder& dec) : dec(dec) {
+}
+
+uint64_t BitDecoder::readBits(size_t count) {
+    if (count > 64) {
+        throw std::logic_error("Only support reading 64 bits at a time");
+    }
+
+    size_t result = 0;
+    while (count > 0) {
+        if (remainingBits == 0) {
+            dec.read_int_to(buffer);
+            remainingBits = 8;
+        }
+        auto bitsToRead = std::min(count, size_t(remainingBits));
+
+        result <<= bitsToRead;
+        result |= getBitsFromBuffer(bitsToRead);
+        count -= bitsToRead;
+        remainingBits -= bitsToRead;
+    };
+
+    return result & ((1 << count) - 1);
+}
+
+bool BitDecoder::readBit() {
+    return bool(readBits(1) & 1);
+}
+
+uint8_t BitDecoder::getMask(size_t bitCount) {
+    return (uint8_t(1) << bitCount) - 1;
+}
+
+uint8_t BitDecoder::getBitsFromBuffer(size_t bitCount) {
+    auto mask = getMask(bitCount);
+    mask <<= (remainingBits - bitCount);
+    auto value = buffer & mask;
+    value >>= (remainingBits - bitCount);
+    return value;
+}
--- src/bit_decoder.h
+++ src/bit_decoder.h
@@ -4,46 +4,17 @@
 
 class BitDecoder {
 public:
-    BitDecoder(Decoder& dec) : dec(dec){};
+    BitDecoder(Decoder& dec);
 
-    uint64_t readBits(size_t count) {
-        if (count > 64) {
-            throw std::logic_error("Only support reading 64 bits at a time");
-        }
+    uint64_t readBits(size_t count);
 
-        size_t result = 0;
-        while (count > 0) {
-            if (remainingBits == 0) {
-                dec.read_int_to(buffer);
-                remainingBits = 8;
-            }
-            auto bitsToRead = std::min(count, size_t(remainingBits));
-
-            result <<= bitsToRead;
-            result |= getBitsFromBuffer(bitsToRead);
-            count -= bitsToRead;
-            remainingBits -= bitsToRead;
-        };
-
-        return result & ((1 << count) - 1);
-    }
-
-    bool readBit() {
-        return bool(readBits(1) & 1);
-    }
+    bool readBit();
 
 protected:
-    static uint8_t getMask(size_t bitCount) {
-        return (uint8_t(1) << bitCount) - 1;
-    }
+    static uint8_t getMask(size_t bitCount);
 
-    uint8_t getBitsFromBuffer(size_t bitCount) {
-        auto mask = getMask(bitCount);
-        mask <<= (remainingBits - bitCount);
-        auto value = buffer & mask;
-        value >>= (remainingBits - bitCount);
-        return value;
-    }
+    uint8_t getBitsFromBuffer(size_t bitCount);
+
     Decoder& dec;
     uint8_t buffer;
     uint8_t remainingBits = 0;
