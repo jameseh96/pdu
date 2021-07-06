1210682cd4c8f3d0b56ac1307d10c9a06fe43a4c
James Harrison <00jamesh@gmail.com>
2021-07-06 17:08:20 +0100

    Make SeriesFilter operators const
    Allow the filter to be reused by returning the matching series refs,
    rather than storing them as a member.

--- src/query/series_filter.h
+++ src/query/series_filter.h
@@ -10,39 +10,34 @@
 class SeriesFilter {
 public:
     using ValueMatcher = std::function<bool(std::string_view)>;
+    using PerLabelRefs = std::map<std::string_view, std::set<size_t>>;
 
     void addFilter(std::string_view key, ValueMatcher valueMatcher) {
         matchers.try_emplace(std::string(key), std::move(valueMatcher));
     }
 
-    void operator()(const Index& index) {
-        for (const auto& postingOffset : index.postings) {
-            (*this)(postingOffset, index);
-        }
-    }
+    std::set<size_t> operator()(const Index& index) const {
+        PerLabelRefs refs;
 
-    void operator()(PostingOffset po, const Index& index) {
-        if (auto itr = matchers.find(po.labelKey); itr != matchers.end()) {
-            const auto& [labelKey, matcher] = *itr;
-            if (matcher(po.labelValue)) {
-                // collect up all references to series which match this
-                // specific label matcher
-                for (const auto& seriesRef : index.getSeriesRefs(po)) {
-                    seriesRefs[labelKey].insert(seriesRef);
-                }
-            }
-        }
-    }
+        // collect all series references by label key matching a provided
+        // matcher.
+        (*this)(index, refs);
 
-    std::set<size_t> getResult() const {
-        if (seriesRefs.empty()) {
+        if (refs.empty()) {
             return {};
         }
-        auto itr = seriesRefs.begin();
+
+        // find the intersection of the references for each label key
+        // e.g., to match
+        //     {__name__=~"foo.*", job="bar"}
+        // intersect all references to series which match the __name__ selector
+        // with those which match the job selector - the resulting set of
+        // references point to series matching the filter.
+        auto itr = refs.begin();
         std::set<size_t> result = itr->second;
         std::set<size_t> tmp;
         ++itr;
-        for (; itr != seriesRefs.end(); ++itr) {
+        for (; itr != refs.end(); ++itr) {
             auto& perLabel = itr->second;
             // intersect the series which matched on each label to find
             // only the series which matched all label filters
@@ -58,11 +53,27 @@ public:
         return result;
     }
 
-    void clear() {
-        seriesRefs.clear();
+private:
+    void operator()(const Index& index, PerLabelRefs& seriesRefs) const {
+        for (const auto& postingOffset : index.postings) {
+            (*this)(postingOffset, index, seriesRefs);
+        }
     }
 
-private:
+    void operator()(PostingOffset po,
+                    const Index& index,
+                    PerLabelRefs& seriesRefs) const {
+        if (auto itr = matchers.find(po.labelKey); itr != matchers.end()) {
+            const auto& [labelKey, matcher] = *itr;
+            if (matcher(po.labelValue)) {
+                // collect up all references to series which match this
+                // specific label matcher
+                for (const auto& seriesRef : index.getSeriesRefs(po)) {
+                    seriesRefs[labelKey].insert(seriesRef);
+                }
+            }
+        }
+    }
+
     std::map<std::string, ValueMatcher, std::less<>> matchers;
-    std::map<std::string_view, std::set<size_t>> seriesRefs;
 };
