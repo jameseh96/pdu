f7b504c3086c0abdc3075a24e10d6aed088e0c02
James Harrison <00jamesh@gmail.com>
2021-08-17 15:46:07 +0100

    Simplify adding an exact match SeriesFilter

---
+++ src/query/series_filter.cc
@@ -0,0 +1,68 @@
+#include "series_filter.h"
+
+namespace pdu::filter {
+
+Filter exactly(std::string expected) {
+    return [expected](std::string_view value) { return value == expected; };
+}
+
+} // namespace pdu::filter
+
+std::set<size_t> SeriesFilter::operator()(const Index& index) const {
+    PerLabelRefs refs;
+
+    // collect all series references by label key matching a provided
+    // matcher.
+    (*this)(index, refs);
+
+    if (refs.empty()) {
+        return {};
+    }
+
+    // find the intersection of the references for each label key
+    // e.g., to match
+    //     {__name__=~"foo.*", job="bar"}
+    // intersect all references to series which match the __name__ selector
+    // with those which match the job selector - the resulting set of
+    // references point to series matching the filter.
+    auto itr = refs.begin();
+    std::set<size_t> result = itr->second;
+    std::set<size_t> tmp;
+    ++itr;
+    for (; itr != refs.end(); ++itr) {
+        auto& perLabel = itr->second;
+        // intersect the series which matched on each label to find
+        // only the series which matched all label filters
+        std::set_intersection(result.begin(),
+                              result.end(),
+                              perLabel.begin(),
+                              perLabel.end(),
+                              std::inserter(tmp, tmp.begin()));
+        std::swap(result, tmp);
+        tmp.clear();
+    }
+
+    return result;
+}
+
+void SeriesFilter::operator()(const Index& index,
+                              PerLabelRefs& seriesRefs) const {
+    for (const auto& postingOffset : index.postings) {
+        (*this)(postingOffset, index, seriesRefs);
+    }
+}
+
+void SeriesFilter::operator()(PostingOffset po,
+                              const Index& index,
+                              PerLabelRefs& seriesRefs) const {
+    if (auto itr = matchers.find(po.labelKey); itr != matchers.end()) {
+        const auto& [labelKey, matcher] = *itr;
+        if (matcher(po.labelValue)) {
+            // collect up all references to series which match this
+            // specific label matcher
+            for (const auto& seriesRef : index.getSeriesRefs(po)) {
+                seriesRefs[labelKey].insert(seriesRef);
+            }
+        }
+    }
+}
--- src/query/series_filter.h
+++ src/query/series_filter.h
@@ -6,6 +6,14 @@
 #include <map>
 #include <set>
 #include <string_view>
+#include <utility>
+
+namespace pdu::filter {
+using Filter = std::function<bool(std::string_view)>;
+
+Filter exactly(std::string expected);
+
+} // namespace pdu::filter
 
 class SeriesFilter {
 public:
@@ -16,64 +24,19 @@ public:
         matchers.try_emplace(std::string(key), std::move(valueMatcher));
     }
 
-    std::set<size_t> operator()(const Index& index) const {
-        PerLabelRefs refs;
-
-        // collect all series references by label key matching a provided
-        // matcher.
-        (*this)(index, refs);
-
-        if (refs.empty()) {
-            return {};
-        }
-
-        // find the intersection of the references for each label key
-        // e.g., to match
-        //     {__name__=~"foo.*", job="bar"}
-        // intersect all references to series which match the __name__ selector
-        // with those which match the job selector - the resulting set of
-        // references point to series matching the filter.
-        auto itr = refs.begin();
-        std::set<size_t> result = itr->second;
-        std::set<size_t> tmp;
-        ++itr;
-        for (; itr != refs.end(); ++itr) {
-            auto& perLabel = itr->second;
-            // intersect the series which matched on each label to find
-            // only the series which matched all label filters
-            std::set_intersection(result.begin(),
-                                  result.end(),
-                                  perLabel.begin(),
-                                  perLabel.end(),
-                                  std::inserter(tmp, tmp.begin()));
-            std::swap(result, tmp);
-            tmp.clear();
-        }
-
-        return result;
+    void addFilter(std::string_view key, std::string value) {
+        using namespace pdu::filter;
+        addFilter(key, exactly(std::move(value)));
     }
+
+    std::set<size_t> operator()(const Index& index) const;
 
 private:
-    void operator()(const Index& index, PerLabelRefs& seriesRefs) const {
-        for (const auto& postingOffset : index.postings) {
-            (*this)(postingOffset, index, seriesRefs);
-        }
-    }
+    void operator()(const Index& index, PerLabelRefs& seriesRefs) const;
 
     void operator()(PostingOffset po,
                     const Index& index,
-                    PerLabelRefs& seriesRefs) const {
-        if (auto itr = matchers.find(po.labelKey); itr != matchers.end()) {
-            const auto& [labelKey, matcher] = *itr;
-            if (matcher(po.labelValue)) {
-                // collect up all references to series which match this
-                // specific label matcher
-                for (const auto& seriesRef : index.getSeriesRefs(po)) {
-                    seriesRefs[labelKey].insert(seriesRef);
-                }
-            }
-        }
-    }
+                    PerLabelRefs& seriesRefs) const;
 
     std::map<std::string, ValueMatcher, std::less<>> matchers;
 };
