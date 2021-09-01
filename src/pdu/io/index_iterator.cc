ebfcb807168bc65a4d2bd44e368f70a30a7aa31e
James Harrison <00jamesh@gmail.com>
2021-09-01 12:21:15 +0100

    Avoid loading .tmp block dirs left by compaction
    If stats are read during compaction, or Prometheus crashed during
    compaction, or compaction simply failed, `<ULID>.tmp` directories may
    be left on disk.
    
    These might duplicate data from other block dirs or the head dir. Skip
    them when loading block dir indexes.

--- src/pdu/io/index_iterator.cc
+++ src/pdu/io/index_iterator.cc
@@ -1,6 +1,7 @@
 #include "index_iterator.h"
 
 #include <boost/filesystem.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 
 IndexIterator::IndexIterator(const boost::filesystem::path& path)
     : path(path), dirIter(this->path) {
@@ -17,6 +18,12 @@ void IndexIterator::advanceToValidIndex() {
     while (dirIter != end(dirIter)) {
         const auto& file = *dirIter;
         const auto& subdir = file.path();
+        if (boost::ends_with(subdir.string(), ".tmp")) {
+            // this is a directory left over during compaction
+            // probably shouldn't read it as it may be partial
+            // and probably duplicates other data
+            continue;
+        }
         const auto& indexFile = subdir / "index";
         if (fs::is_regular_file(indexFile)) {
             index = loadIndex(indexFile.string());
