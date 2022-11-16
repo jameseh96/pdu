ceec25201a8edfc37e15b32d8c0e122b1de4e7ee
James Harrison <00jamesh@gmail.com>
2022-11-16 13:48:46 +0000

    Cache value of numpy_available
    It will not change within a given run (... normally).

--- src/pypdu/pypdu_numpy_check.cc
+++ src/pypdu/pypdu_numpy_check.cc
@@ -1,10 +1,15 @@
 #include "pypdu_numpy_check.h"
 
-bool numpy_available(py::module m) {
+bool numpy_available_inner(py::module m) {
     try {
         pybind11::module_::import("numpy");
         return true;
     } catch (...) {
         return false;
     }
+}
+
+bool numpy_available(py::module m) {
+    static bool available = numpy_available_inner(m);
+    return available;
 }
