b1b8806d2b68ada355e384eb179fc658f31ebbe5
James Harrison <00jamesh@gmail.com>
2024-10-16 18:02:24 +0100

    Correct fortify source check

--- src/musl_feature_test/foo.cpp
+++ src/musl_feature_test/foo.cpp
@@ -1,3 +1,5 @@
-#include <stdio.h>
 #include <string>
-int foo() { vsnprintf("a", 1, "x", nullptr); volatile std::string v = std::to_string(1); return 0; }
+int foo() {
+    volatile std::string v = std::to_string(1);
+    return 0;
+}
